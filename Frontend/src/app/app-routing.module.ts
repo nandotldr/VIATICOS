36abd4952d2a226e0ae6c24a6949eb95c4ea3a1e
launderex619 <carlos12150@hotmail.com>
2020-01-29 17:34:28 -0600

    Creado login (funcionalidad)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
@@ -15,10 +16,9 @@ const routes: Routes = [
     loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   },
   {
-    path: 'new-user',
-    loadChildren: () => import('./pages/new-user/new-user.module').then( m => m.NewUserPageModule)
+    path: '**',
+    redirectTo: 'login'
   },
-
 ];
 
 @NgModule({

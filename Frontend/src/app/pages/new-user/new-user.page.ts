f8d9f9efc56ce679952fb20f78fe3990bfba7448
Miguel <jmiguel_rs@hotmail.com>
2020-01-29 15:01:02 -0600

    Creacion archivos front y navbar verde

---
+++ Frontend/src/app/pages/new-user/new-user.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.page.html',
+  styleUrls: ['./new-user.page.scss'],
+})
+export class NewUserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

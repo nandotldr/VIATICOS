0d001caae2de68115d95350f5e224646bc9178b9
emmanuel8875 <emmanuel8875@gmail.com>
2020-01-29 19:32:16 -0600

    corregido error en una ruta

--- Backend/src/routes/solicitud_viatico.js
+++ Backend/src/routes/solicitud_viatico.js
@@ -5,7 +5,7 @@ const oauth = require('../middlewares/credentials');
 const router = express.Router();
 
 router.post('/', oauth('P', 'J', 'A', 'S'),solicitud_viaticoCtl.insert);
-router.get('/', solicitud_viaticoCtl.selectAll);
+//router.get('/', solicitud_viaticoCtl.selectAll);
 router.get('/:id', oauth('P', 'J', 'A', 'S'),solicitud_viaticoCtl.select);
 router.put('/', oauth('P'), solicitud_viaticoCtl.update);
 //router.delete('/', solicitud_viaticoCtl.delete);

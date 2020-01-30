5d3de6e1bed035ef8f96f04581fcd4de81107d8e
emmanuel8875 <emmanuel8875@gmail.com>
2020-01-29 18:37:41 -0600

    Todas las rutas disponibles para usar

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -9,17 +9,17 @@ router.get('/', auth(), (req, res) => {
 
 
 router.use('/factura', require('./factura'));
-// router.use('/gasto', require('./gasto'));
-// router.use('/historial_usuario', require('./historial_usuario'));
-// router.use('/informe_actividades', require('./informe_actividades'));
+router.use('/gasto', require('./gasto'));
+router.use('/historial_usuario', require('./historial_usuario'));
+router.use('/informe_actividades', require('./informe_actividades'));
 router.use('/login', require('./login'));
-// router.use('/municipio', require('./municipio'));
-// router.use('/pais', require('./pais'));
-// router.use('/programa_trabajo', require('./programa_trabajo'));
-// router.use('/solicitud_comision', require('./solicitud_ comision'));
-// router.use('/solicitud_viatico', require('./solicitud_viatico'));
-// router.use('/usuario', require('./usuario'));
-// router.use('/viatico_proyecto', require('./viatico_proyecto'));
+router.use('/municipio', require('./municipio'));
+router.use('/pais', require('./pais'));
+router.use('/programa_trabajo', require('./programa_trabajo'));
+router.use('/solicitud_comision', require('./solicitud_ comision'));
+router.use('/solicitud_viatico', require('./solicitud_viatico'));
+router.use('/usuario', require('./usuario'));
+router.use('/viatico_proyecto', require('./viatico_proyecto'));
 
 
 

47a96da18520ef19a6f23ce069ee668a11d2bdc6
launderex619 <carlos12150@hotmail.com>
2020-01-30 23:13:26 -0600

    Rutas arregladas

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
@@ -18,22 +17,6 @@ const routes: Routes = [
   {
     path: 'create-user',
     loadChildren: () => import('./pages/create-user/create-user.module').then( m => m.CreateUserPageModule)
-  },
-  {
-    path: 'create-comision',
-    loadChildren: () => import('./pages/create-comision/create-comision.module').then( m => m.CreateComisionPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule)
-  },
-  {
-    path: 'history',
-    loadChildren: () => import('./pages/history/history.module').then(m => m.HistoryPageModule)
-  },
-  {
-    path: 'viatico',
-    loadChildren: () => import('./pages/viatico/viatico.module').then(m => m.ViaticoPageModule)
   },
   {
     path: '**',
--- Frontend/src/app/pages/home/home-routing.module.ts
+++ Frontend/src/app/pages/home/home-routing.module.ts
@@ -6,7 +6,29 @@ import { HomePage } from './home.page';
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'profile'
+      },
+      {
+        path: 'create-comision',
+        loadChildren: () => import('./create-comision/create-comision.module').then( m => m.CreateComisionPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: 'history',
+        loadChildren: () => import('./history/history.module').then(m => m.HistoryPageModule)
+      },
+      {
+        path: 'viatico',
+        loadChildren: () => import('./viatico/viatico.module').then(m => m.ViaticoPageModule)
+      },
+    ]
   }
 ];
 

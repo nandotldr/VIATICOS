6348e2c7245507ac8182c73741c8f68fb0cc7f0d
Iszael Gonzalez <iszael.gonzalez@alumnos.udg.mx>
2020-01-30 23:57:54 -0600

    se arreglo rutas crearPrograma y modificarPrograma

--- Backend/src/routes/solicitud_ comision.js
+++ Backend/src/routes/solicitud_ comision.js
@@ -4,8 +4,8 @@ const solicitud_comisionCtl = require('../controllers/solicitud_comision');
 const router = express.Router();
 const auth = require('../middlewares/credentials');
 
-router.post('/',auth(),solicitud_comisionCtl.crearSolicitudComision);
-router.get('/:id', auth(),solicitud_comisionCtl.consultarSolicitudComison);
+router.post('/',solicitud_comisionCtl.crearSolicitudComision);
+router.get('/:id',solicitud_comisionCtl.consultarSolicitudComison);
 router.get('/',solicitud_comisionCtl.historialComisones);
 router.put('/', solicitud_comisionCtl.modificarComision);
 
--- Backend/src/routes/viatico_proyecto.js
+++ Backend/src/routes/viatico_proyecto.js
@@ -3,10 +3,10 @@ const viatico_proyectoCtl = require('../controllers/viatico_proyecto');
 const auth = require('../middlewares/credentials');
 const router = express.Router();
 
-router.post('/', auth(), viatico_proyectoCtl.crearViaticoProyecto);
-router.get('/:id', auth(), viatico_proyectoCtl.verViaticoProyecto);
+router.post('/', viatico_proyectoCtl.crearViaticoProyecto);
+router.get('/:id', viatico_proyectoCtl.verViaticoProyecto);
 router.put('/', viatico_proyectoCtl.modificarViaticoProyecto);
-router.delete('/', auth(), viatico_proyectoCtl.eliminarProyecto);
+router.delete('/', viatico_proyectoCtl.eliminarProyecto);
 
 router.post('/', viatico_proyectoCtl.insert);
 router.get('/', viatico_proyectoCtl.selectAll);

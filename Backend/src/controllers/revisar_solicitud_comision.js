f6b87cec24d6b2f2f381c04d8112d33c753f1d45
Jairo <jairoj_glez@hotmail.com>
2020-02-04 00:19:46 -0600

    Rutas arregladas y collection en postman
    Se arreglaron algunas rutas por terminar y se agrego la coleccion completa de postman

--- Backend/src/controllers/revisar_solicitud_comision.js
+++ Backend/src/controllers/revisar_solicitud_comision.js
@@ -6,7 +6,7 @@ const pool = require('../database');
  * respectivamente.
  */
 module.exports = {
-    consultarSolicitudesComison:async (req, res) => {     
+    consultarSolicitudesComision: async(req, res) => {
         try {
             //Verificar tipo de usuario
             const existeUsuario = await pool.query('SELECT codigo, tipo_usuario,area_adscripcion FROM usuario WHERE codigo=?', [req.user.codigo]);
@@ -14,23 +14,20 @@ module.exports = {
                 return res.json({ ok: false, mensaje: "Este usuario no existe" });
             }
             //si usuario es A mostrar todas las solocitudes de comison en status 3
-            if(existeUsuario[0].tipo_usuario =='A')
-            {
+            if (existeUsuario[0].tipo_usuario == 'A') {
                 const comision = await pool.query('SELECT c.id, c.status, u.codigo, u.area_adscripcion,c.fecha_solicitud , c.nombre_comision,concat(u.nombres," ",u.apellidos) as nombre  FROM solicitud_comision AS c INNER JOIN usuario as u ON u.codigo=c.id_usuario WHERE c.status =3');
-                    if (comision.length < 1) res.json({ ok: false, mensaje: "No hay comisiones por aceptar" });
-                    
-                return res.json({ok:true, body: comision});
+                if (comision.length < 1) res.json({ ok: false, mensaje: "No hay comisiones por aceptar" });
 
+                return res.json({ ok: true, body: comision });
+
+            } else if (existeUsuario[0].tipo_usuario == 'J') {
+                const comision = await pool.query('SELECT c.id, c.status, u.codigo, u.area_adscripcion,c.fecha_solicitud , c.nombre_comision,concat(u.nombres," ",u.apellidos) as nombre  FROM solicitud_comision AS c INNER JOIN usuario as u ON u.codigo=c.id_usuario WHERE c.status =1 AND u.area_adscripcion = ? group by c.id', [existeUsuario[0].area_adscripcion]);
+                if (comision.length < 1) res.json({ ok: false, mensaje: "No hay comisiones por aceptar" });
+
+                return res.json({ ok: true, body: comision });
             }
-            else if(existeUsuario[0].tipo_usuario =='J')
-            {
-                const comision = await pool.query('SELECT c.id, c.status, u.codigo, u.area_adscripcion,c.fecha_solicitud , c.nombre_comision,concat(u.nombres," ",u.apellidos) as nombre  FROM solicitud_comision AS c INNER JOIN usuario as u ON u.codigo=c.id_usuario WHERE c.status =1 AND u.area_adscripcion = ? group by c.id',[existeUsuario[0].area_adscripcion]);
-                    if (comision.length < 1) res.json({ ok: false, mensaje: "No hay comisiones por aceptar" });
-                    
-                return res.json({ok:true, body: comision});
-            }
-            res.json({ok: false, mensaje: "Funcion no disponible para tu usuario"})
-            //si usuario es J mostrar las solicitudes de su dependencia
+            res.json({ ok: false, mensaje: "Funcion no disponible para tu usuario" })
+                //si usuario es J mostrar las solicitudes de su dependencia
         } catch (error) {
             return res.json({ ok: false, mensaje: error });
         }
@@ -39,47 +36,45 @@ module.exports = {
 
     modificarComision: async(req, res) => {
         //verificar que no este en status cancelado =-1, revision = 1, aceptado por J =3, aceptado por A= 5 o finalizado
-        try {            
-            var sqlSolComision ='SELECT c.id, c.status, u.codigo, c.fecha_solicitud , concat(u.nombres," ",u.apellidos) as nombre, u.tipo_usuario FROM solicitud_comision AS c INNER JOIN usuario as u ON u.codigo = ? WHERE c.id = ? AND (c.status =1 OR c.status=3)';
-            const verificarComision = await pool.query(sqlSolComision, [req.user.codigo,req.body.id]);
+        try {
+            var sqlSolComision = 'SELECT c.id, c.status, u.codigo, c.fecha_solicitud , concat(u.nombres," ",u.apellidos) as nombre, u.tipo_usuario FROM solicitud_comision AS c INNER JOIN usuario as u ON u.codigo = ? WHERE c.id = ? AND (c.status =1 OR c.status=3)';
+            const verificarComision = await pool.query(sqlSolComision, [req.user.codigo, req.body.id]);
             if (verificarComision.length < 1) {
-                return res.json({ ok: false, mensaje: "No se puede modificar comison" });
-            }  
-            var modificarComision='UPDATE solicitud_comision SET ? WHERE id = ?';
+                return res.json({ ok: false, mensaje: "No se puede modificar comision" });
+            }
+            var modificarComision = 'UPDATE solicitud_comision SET ? WHERE id = ?';
             //si usuario =J modifcar fecha revisado, nombre revisado, comentario rechazo
             //si usuario =A modificar fecha_aceptado, nombre aceptado, comentario rechazo
-            if(verificarComision[0].tipo_usuario =='J' && verificarComision[0].status ==1){
+            if (verificarComision[0].tipo_usuario == 'J' && verificarComision[0].status == 1) {
                 pool.query(modificarComision, [{
                     fecha_modificacion: new Date(),
-                    fecha_revisado:new Date(),
+                    fecha_revisado: new Date(),
                     nombre_revisado: verificarComision[0].nombre,
                     comentario_rechazo: req.body.comentario_rechazo,
                     status: req.body.status,
-                },req.body.id], (errorModificar, modificarComision) => {
-                    if (errorModificar) return res.json({ok: false, mensaje:errorModificar});
-                        
-                    });
-                    return res.json({ ok: true, mensaje: "Comision modificada" });
-                 
-            }
-            else if (verificarComision[0].tipo_usuario =='A' && verificarComision[0].status ==3)
-            {
+                }, req.body.id], (errorModificar, modificarComision) => {
+                    if (errorModificar) return res.json({ ok: false, mensaje: errorModificar });
+
+                });
+                return res.json({ ok: true, mensaje: "Comision modificada" });
+
+            } else if (verificarComision[0].tipo_usuario == 'A' && verificarComision[0].status == 3) {
                 pool.query(modificarComision, [{
                     fecha_modificacion: new Date(),
-                    fecha_aceptado:new Date(),
-                    nombre_aceptado: verificarComision[0].nombre ,
+                    fecha_aceptado: new Date(),
+                    nombre_aceptado: verificarComision[0].nombre,
                     comentario_rechazo: req.body.comentario_rechazo,
                     status: req.body.status,
-                },req.body.id], (errorModificar, modificarComision) => {
-                    if (errorModificar) return res.json({ok: false, mensaje:errorModificar});
-                    });
-                    return res.json({ ok: true, mensaje: "Comision modificada" });
-                
+                }, req.body.id], (errorModificar, modificarComision) => {
+                    if (errorModificar) return res.json({ ok: false, mensaje: errorModificar });
+                });
+                return res.json({ ok: true, mensaje: "Comision modificada" });
+
             }
-            
-            res.json({ok:false, mensaje:"No se hizo la revision correcta"});
+
+            res.json({ ok: false, mensaje: "No se hizo la revision correcta" });
         } catch (error) {
-            return res.json({ ok: false, mensaje: error});
+            return res.json({ ok: false, mensaje: error });
         }
     },
 }

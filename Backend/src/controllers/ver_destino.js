d67bfe844df490cd3e3e46ee2c7a36cfd3bc4858
Jairo <jairoj_glez@hotmail.com>
2020-02-10 19:39:50 -0600

    Estados y continenetes

--- Backend/src/controllers/ver_destino.js
+++ Backend/src/controllers/ver_destino.js
@@ -12,14 +12,14 @@ module.exports = {
         try {
             var tipo_comision = req.body.tipo_comision;
             if (tipo_comision == 0) {
-                pool.query('SELECT * FROM pais', (errordestino, destino) => {
+                pool.query('SELECT p.nombre as pais, c.nombre as continente , p.zona, p.id as id_pais FROM pais as p INNER JOIN continente as c on c.id= p.id_continente group by p.id', (errordestino, destino) => {
                     if (errordestino) return res.json({ ok: false, mensaje: errordestino });
                     if (destino.length < 1) return res.json({ ok: false, mensaje: "Error" });
 
                     res.json({ ok: true, body: destino });
                 });
             } else if (tipo_comision == 1) {
-                pool.query('SELECT * FROM municipio', (errordestino, destino) => {
+                pool.query('SELECT m.nombre as municipio, e.nombre as estado , m.zona, m.id as id_municipio FROM viaticos.municipio as m INNER JOIN estado as e on e.id= m.id_estado group by m.id', (errordestino, destino) => {
                     if (errordestino) return res.json({ ok: false, mensaje: errordestino });
                     if (destino.length < 1) return res.json({ ok: false, mensaje: "Error" });
 

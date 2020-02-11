d62ce9beb6c5c916aa992cc0df10a16239ba9963
Jairo <jairoj_glez@hotmail.com>
2020-02-11 16:46:13 -0600

    Update ver_destino.js

--- Backend/src/controllers/ver_destino.js
+++ Backend/src/controllers/ver_destino.js
@@ -13,14 +13,14 @@ module.exports = {
             var tipo_comision = req.body.tipo_comision;
             var id_destino = req.body.id
             if (tipo_comision == 0) {
-                pool.query('SELECT * FROM pais WHERE id_continente = ?', id_destino, (errordestino, destino) => {
+                pool.query('SELECT id, nombre FROM pais WHERE id_continente = ?', id_destino, (errordestino, destino) => {
                     if (errordestino) return res.json({ ok: false, mensaje: errordestino });
                     if (destino.length < 1) return res.json({ ok: false, mensaje: "Error" });
 
                     res.json({ ok: true, body: destino });
                 });
             } else if (tipo_comision == 1) {
-                pool.query('SELECT * FROM municipio WHERE id_estado = ?', id_destino, (errordestino, destino) => {
+                pool.query('SELECT id, nombre FROM municipio WHERE id_estado = ?', id_destino, (errordestino, destino) => {
                     if (errordestino) return res.json({ ok: false, mensaje: errordestino });
                     if (destino.length < 1) return res.json({ ok: false, mensaje: "Error" });
 

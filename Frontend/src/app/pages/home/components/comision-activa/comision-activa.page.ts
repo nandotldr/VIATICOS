83fa0e56454b953643aa082ff696697683db6c9b
Isaac <legato.isaac@gmail.com>
2020-02-10 21:17:08 -0600

    Avances a comision-activa

--- Frontend/src/app/pages/home/components/comision-activa/comision-activa.page.ts
+++ Frontend/src/app/pages/home/components/comision-activa/comision-activa.page.ts
@@ -1,5 +1,6 @@
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavParams} from '@ionic/angular';
+import { ModalController, NavParams, ToastController } from '@ionic/angular';
 import { AuthService } from '../../../../services/auth.service';
 import { NavController } from "@ionic/angular";
 import { formatDate } from '@angular/common';
@@ -12,11 +13,21 @@ import { formatDate } from '@angular/common';
 export class ComisionActivaPage implements OnInit {
   comision = '1';
   comi = '';
+  fgModify: FormGroup;
   constructor(private modalController: ModalController,
               private navParams: NavParams,
-              private authService: AuthService,
-              private navCtrl: NavController) {
-
+              private auth: AuthService,
+              private navCtrl: NavController,
+              private toastController: ToastController,
+              private formBuilder: FormBuilder) {
+                this.ionViewWillEnter();
+                this.fgModify = this.formBuilder.group({
+                  nombres: new FormControl(this.comision, []),
+                  apellidos: new FormControl(this.comision, []),
+                  area_adscripcion: new FormControl(this.comision, []),
+                  plaza_laboral: new FormControl(this.comision, []),
+                  nss: new FormControl(this.comision, [])
+                });
   }
   ionViewWillEnter() {
     this.comision = this.navParams.get('comision');
@@ -31,7 +42,7 @@ export class ComisionActivaPage implements OnInit {
     await this.modalController.dismiss(result);
   }
   async getComision(comision_id) {
-    const resp = await this.authService.getComision(comision_id);
+    const resp = await this.auth.getComision(comision_id);
     console.log(resp);
     if (resp) {
       resp.fecha_inicio = formatDate(resp.fecha_inicio, 'yyyy-MM-dd', 'en');
@@ -43,11 +54,45 @@ export class ComisionActivaPage implements OnInit {
 
   }
 
+  async modifyUsuario(){
+    if (this.fgModify.valid) {
+      console.log(this.fgModify.value);
+      const resp = await this.auth.modifyUsuario(this.fgModify.value);
+      if (resp) {
+        this.presentToastSuccess();
+      } else {
+        console.log(resp);
+        this.presentToast();
+      }
+    } else {
+      this.presentToast();
+    }
+    this.closeModal();
+  }
+
   closeModal() {
     this.modalController.dismiss();
   }
   ngOnInit() {
 
+  }
+
+  async presentToast() { 
+    const toast = await this.toastController.create({
+      message: 'Datos no Validos',
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  async presentToastSuccess() {
+    const toast = await this.toastController.create({
+      message: 'Usuario Modificado.',
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }

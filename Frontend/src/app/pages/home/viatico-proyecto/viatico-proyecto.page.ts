5c2aac299a00879aee211d5c0573f764b5cb1d3d
Alex N <alejandro.noriega@alumnos.udg.mx>
2020-03-02 20:04:54 -0600

    Arreglando vista

--- Frontend/src/app/pages/home/viatico-proyecto/viatico-proyecto.page.ts
+++ Frontend/src/app/pages/home/viatico-proyecto/viatico-proyecto.page.ts
@@ -1,7 +1,7 @@
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { ToastController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,8 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./viatico-proyecto.page.scss'],
 })
 export class ViaticoProyectoPage implements OnInit {
-
-  guardado = false;
+  
   comision: number;
   viatico: any;
   viaticoProyecto: any;
@@ -23,17 +22,18 @@ export class ViaticoProyectoPage implements OnInit {
   }
   fgCreate: FormGroup;
   id_solicitud_viatico: number;
-  
+
 
   constructor(
     private formBuilder: FormBuilder,
     private auth: AuthService,
     public toastController: ToastController,
+    public alertController: AlertController,
     private activatedRoute: ActivatedRoute
-  ) { 
+  ) {
     this.comision = +this.activatedRoute.snapshot.paramMap.get('id_comision');
     this.fgCreate = this.formBuilder.group({
-      id_solicitud_viatico: new FormControl(this.id_solicitud_viatico, [Validators.required]),
+      id_solicitud_viatico: new FormControl(this.id_solicitud_viatico, []),
       numero_proyecto: new FormControl('', [Validators.required]),
       cantidad: new FormControl('', [Validators.required])
     });
@@ -41,18 +41,17 @@ export class ViaticoProyectoPage implements OnInit {
 
   async ngOnInit() {
     await this.getViaticos();
-    this.getViaticosProyecto();
   }
 
   async saveViaticoProyecto(){
     if (this.fgCreate.valid) {
-     
+
       const resp = await this.auth.saveViaticoProyecto(this.fgCreate.value).toPromise();
       console.log(resp);
       if (resp['ok']) {
         this.presentToast('Guardado correctamente');
-        
-        
+
+
       } else {
         this.presentToast('Error');
       }
@@ -60,30 +59,6 @@ export class ViaticoProyectoPage implements OnInit {
       this.presentToast('Datos no Validos');
     }
 
-  }
-
-  async sendViaticoProyecto(){
-    if (this.fgCreate.valid) {
-    if (confirm("¿Está seguro de enviar los datos? Una vez enviados no podrán ser modificados")) {
-      this.viatico_proyecto = this.fgCreate.value;
-      this.viatico_proyecto.status = 1;
-      const resp = await this.auth.createViaticoProyecto(
-        this.viatico_proyecto.id_solicitud_viatico,
-        this.viatico_proyecto.numero_proyecto,
-        this.viatico_proyecto.cantidad,
-        this.viatico_proyecto.status,
-      ).toPromise();
-        console.log(resp);
-      if (resp['ok']) {
-        this.presentToast('Eviado de forma exitosa');
-        this.guardado = true; 
-      } else {
-        this.presentToast('Error');
-      }
-    }
-  } else {
-    this.presentToast('Viatico Proyecto no capturado');
-  }
   }
 
   async presentToast(message) {
@@ -102,19 +77,29 @@ export class ViaticoProyectoPage implements OnInit {
       this.viatico = resp['body'];
       this.id_solicitud_viatico = this.viatico.folio;
     } else {
-      
     }
   }
 
-  async getViaticosProyecto(){
-    const resp = await this.auth.getViaticoProyecto(this.id_solicitud_viatico).toPromise();
-    console.log("viapro", resp);
-    
-    if (resp['ok']) {
-      this.guardado = true;
-      this.viaticoProyecto = resp['body'];
-     } //else {
-    //   this.presentToast(resp['mensaje']);
-    // }
+  async alertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Enviar Proyecto',
+      message: '¿Está seguro de enviar los datos? Una vez enviados no podrán ser modificados',
+      buttons: [
+        {
+          text: 'Si',
+          handler: () => {
+            this.saveViaticoProyecto();
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {}
+        }
+      ]
+    });
+    await alert.present();
+    // alert.onDidDismiss().then(() => this.get());
   }
 }

e7daa72b50972aeb30e6dacfa7e2d09e09fdcda5
Isaac <legato.isaac@gmail.com>
2020-03-02 23:58:48 -0600

    Revisar informe modal ya charcha y revisar informe ya deberia pasar el viatico a status 7

--- Backend/src/routes/informe_actividades.js
+++ Backend/src/routes/informe_actividades.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', auth('P', 'J', 'A', 'S'), informe_actividadesCtl.crearInforme);
 router.get('/', auth('P', 'J', 'A', 'S'), informe_actividadesCtl.historialInforme);
-router.get('/:id', auth('P', 'J', 'A', 'S'), informe_actividadesCtl.verInforme);
+router.get('/:id', auth('P', 'J', 'A', 'S', 'F'), informe_actividadesCtl.verInforme);
 router.put('/', auth('P', 'J', 'A', 'S'), informe_actividadesCtl.modificarInforme);
 
 // Rutas extras del controlador como archivos, etc.

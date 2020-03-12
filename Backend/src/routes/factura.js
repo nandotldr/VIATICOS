503dd7ae0d7567cf2a7f5fef69d8f88606c45ac2
Jairo Gonzalez <jairoj_glez@hotmail.com>
2020-03-12 15:36:03 -0600

    Ruta de subir archivo a factura terminada

--- Backend/src/routes/factura.js
+++ Backend/src/routes/factura.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const facturaCtl = require('../controllers/factura');
 const oauth = require('../middlewares/credentials');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
@@ -9,5 +11,7 @@ router.get('/', oauth('P', 'J', 'A', 'S', 'F'), facturaCtl.selectfactura);
 router.put('/', oauth('P', 'J', 'A', 'S'), facturaCtl.modificarfactura);
 
 // Rutas extras del controlador como archivos, etc.
+
+router.post('/subir/factura', [oauth('P', 'J', 'A', 'S'), upload.single('archivo')], facturaCtl.subirFactura);
 
 module.exports = router;
